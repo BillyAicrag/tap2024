0040760cb9851286584209784f4d8e85a0090cd5
Billy <bilyaicrag6412@gmail.com>
2024-02-06 18:26:37 -0600

    Screen Cuadro Magico

---
+++ src/main/java/org/example/tap2024/vistas/CuadroMagico.java
@@ -0,0 +1,20 @@
+package org.example.tap2024.vistas;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class CuadroMagico extends Stage {
+
+    private Scene escena;
+    public CuadroMagico(){
+        this.setTitle("Cuadro Mágico");
+        this.setScene(new Scene(new Button("Da click")));
+        this.show();
+    }
+
+    private void CrearUI(){
+        escena = new Scene(new Button("Da click"));
+
+    }
+}
